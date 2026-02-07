beeeed4b92f22dea4587cdf41d77ef3253742c3b
ehasun15019 <ehasunulislam0202@gmail.com>
2026-02-07 12:47:44 +0600

    learning how to upload image in the ImageKit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const multer = require("multer");
+const uploadFile = require("./service/storage.service");
 
 // middleware
 app.use(express.json());
@@ -12,7 +13,11 @@ const upload = multer({ storage: multer.memoryStorage() })
 /* API Created start */
 app.post("/create-post", upload.single("image"), async(req, res) => {
     console.log(req.body);
-    console.log(req.file)
+    console.log(req.file);
+
+    const result = await uploadFile(req.file.buffer);
+
+    console.log(result);
 })
 /* API Created end */
 
